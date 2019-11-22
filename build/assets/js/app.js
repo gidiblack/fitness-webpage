3fec5ba0d26558dceb49b1cf36854f77cc903ce2
Tha Gidi <you@example.com>
2019-11-22 17:07:23 +0100

    intersection observer added to navigation

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -4,5 +4,21 @@ $(document).ready(function(){
         $("body").toggleClass('show');
     });
 
+    let navBar = document.querySelector("#nav-bar");
+    let sectionOne = document.querySelector("header");
+    let sectionOneOptions = {
+        rootMargin: "-500px 0px 0px 0px"
+    };
 
+    let sectionOneObserver = new IntersectionObserver(function(entries, sectionOneObserver){
+        entries.forEach(entry =>{
+            if (!entry.isIntersecting){
+                navBar.classList.add("scrolled");
+            } else {
+                navBar.classList.remove("scrolled");
+            }
+        });
+    }, sectionOneOptions);
+
+    sectionOneObserver.observe(sectionOne);
 });
