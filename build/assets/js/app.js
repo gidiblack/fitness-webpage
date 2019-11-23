d28fc3b27478c6e580cfd9d4a2b68ff3957a72d0
Tha Gidi <you@example.com>
2019-11-23 16:18:21 +0100

    mobile-responsiveness implemented

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -1,9 +1,20 @@
+window.sr=ScrollReveal();
+
 $(document).ready(function(){
+
+    // toggle popup modal
     $(".button, .close").click(function(){
         $("#signUpModal").fadeToggle('slow', 'linear');
         $("body").toggleClass('show');
     });
 
+    // toggle mobile nav menu
+    $('.menu-toggle').click(function(){
+        $('.responsive-nav').toggleClass("unfold");
+        $(this).toggleClass("close-menu");
+    });
+
+    // change navigation on scroll
     let navBar = document.querySelector("#nav-bar");
     let sectionOne = document.querySelector("header");
     let sectionOneOptions = {
@@ -21,4 +32,60 @@ $(document).ready(function(){
     }, sectionOneOptions);
 
     sectionOneObserver.observe(sectionOne);
+
+    sr.reveal("#services div.cardio", {
+        duration: 800,
+        distance: '150px',
+        origin: 'left'
+    });
+
+    sr.reveal("#services div.yoga", {
+        duration: 800,
+        delay: 300,
+        distance: '150px',
+        origin: 'bottom'
+    });
+
+    sr.reveal("#services div.power", {
+        duration: 800,
+        delay: 600,
+        distance: '150px',
+        origin: 'right'
+    });
+
+    sr.reveal("#team div.team-1", {
+        duration: 800,
+        delay: 200,
+        distance: '150px',
+        origin: 'left'
+    });
+
+    sr.reveal("#team div.team-2", {
+        duration: 800,
+        delay: 400,
+        distance: '150px',
+        origin: 'bottom'
+    });
+
+    sr.reveal("#team div.team-3", {
+        duration: 800,
+        delay: 800,
+        distance: '150px',
+        origin: 'right'
+    });
+
+    sr.reveal("#pricing div.blue-price", {
+        duration: 800,
+        delay: 200,
+        distance: '150px',
+        origin: 'left'
+    });
+    
+    sr.reveal("#pricing div.gray-price", {
+        duration: 800,
+        delay: 400,
+        distance: '150px',
+        origin: 'left'
+    });
+
 });
